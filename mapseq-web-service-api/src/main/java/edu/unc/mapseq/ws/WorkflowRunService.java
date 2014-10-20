b62bda65bbee8fa47e08107760500f3b8178fa90
Jason Reilly <jdr0887@gmail.com>
2014-10-20 15:47:07 -0400

    cleaning

--- mapseq-web-service-api/src/main/java/edu/unc/mapseq/ws/WorkflowRunService.java
+++ mapseq-web-service-api/src/main/java/edu/unc/mapseq/ws/WorkflowRunService.java
@@ -11,12 +11,10 @@ import javax.jws.soap.SOAPBinding.Use;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import edu.unc.mapseq.dao.MaPSeqDAOException;
 import edu.unc.mapseq.dao.model.WorkflowRun;
