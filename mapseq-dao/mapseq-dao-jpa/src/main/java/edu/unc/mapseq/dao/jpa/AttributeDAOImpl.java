524c2b147fdde5b3e39f48ea205ee422707fad73
Jason Reilly <jdr0887@gmail.com>
2014-10-20 15:46:47 -0400

    cleaning

--- mapseq-dao/mapseq-dao-jpa/src/main/java/edu/unc/mapseq/dao/jpa/AttributeDAOImpl.java
+++ mapseq-dao/mapseq-dao-jpa/src/main/java/edu/unc/mapseq/dao/jpa/AttributeDAOImpl.java
@@ -1,14 +1,9 @@
 package edu.unc.mapseq.dao.jpa;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import edu.unc.mapseq.dao.AttributeDAO;
 import edu.unc.mapseq.dao.model.Attribute;
 
 public class AttributeDAOImpl extends BaseDAOImpl<Attribute, Long> implements AttributeDAO {
-
-    private final Logger logger = LoggerFactory.getLogger(AttributeDAOImpl.class);
 
     public AttributeDAOImpl() {
         super();
