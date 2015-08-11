272151709f0a9182a11c3c8d106b4fc142ef22d3
Jason Reilly <jdr0887@gmail.com>
2015-08-11 13:13:12 -0400

    initial commit

---
+++ mapseq-module-api/src/test/java/edu/unc/mapseq/module/EchoModule.java
@@ -0,0 +1,57 @@
+package edu.unc.mapseq.module;
+
+import java.io.File;
+
+import javax.validation.constraints.NotNull;
+
+import edu.unc.mapseq.module.annotations.Application;
+import edu.unc.mapseq.module.annotations.Executable;
+import edu.unc.mapseq.module.annotations.InputArgument;
+import edu.unc.mapseq.module.annotations.InputValidations;
+import edu.unc.mapseq.module.annotations.OutputArgument;
+import edu.unc.mapseq.module.annotations.OutputValidations;
+import edu.unc.mapseq.module.constraints.FileIsNotEmpty;
+
+/**
+ * 
+ * @author jdr0887
+ */
+@Application(name = "EchoModule")
+@Executable(value = "/bin/echo")
+public class EchoModule extends Module {
+
+    @NotNull(message = "message is null", groups = InputValidations.class)
+    @InputArgument
+    private String message;
+
+    @NotNull(message = "output is null", groups = InputValidations.class)
+    @FileIsNotEmpty(message = "output is empty", groups = OutputValidations.class)
+    @OutputArgument(redirect = true)
+    private File output;
+
+    public EchoModule() {
+        super();
+    }
+
+    @Override
+    public Class<?> getModuleClass() {
+        return EchoModule.class;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public File getOutput() {
+        return output;
+    }
+
+    public void setOutput(File output) {
+        this.output = output;
+    }
+
+}
