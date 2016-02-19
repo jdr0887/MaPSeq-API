4eae1323e9d076ae01443535e3a87c34de31049c
Jason Reilly <jdr0887@gmail.com>
2016-02-19 11:17:28 -0500

    removing comments

--- mapseq-workflow-api/src/main/java/edu/unc/mapseq/workflow/WorkflowException.java
+++ mapseq-workflow-api/src/main/java/edu/unc/mapseq/workflow/WorkflowException.java
@@ -1,15 +1,7 @@
 package edu.unc.mapseq.workflow;
 
-/**
- * 
- * @author jdr0887
- * 
- */
 public class WorkflowException extends Exception {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 9004294363074403060L;
 
     public WorkflowException() {
