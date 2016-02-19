953870cc6422a19b8219d804c641ed22b0aae58e
Jason Reilly <jdr0887@gmail.com>
2016-02-19 11:17:07 -0500

    formatting

--- mapseq-workflow-api/src/main/java/edu/unc/mapseq/workflow/WorkflowException.java
+++ mapseq-workflow-api/src/main/java/edu/unc/mapseq/workflow/WorkflowException.java
@@ -8,8 +8,8 @@ package edu.unc.mapseq.workflow;
 public class WorkflowException extends Exception {
 
     /**
-	 * 
-	 */
+     * 
+     */
     private static final long serialVersionUID = 9004294363074403060L;
 
     public WorkflowException() {
