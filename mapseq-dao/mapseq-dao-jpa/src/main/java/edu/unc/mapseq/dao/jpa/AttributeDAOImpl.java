c43e55e4ec4710bdf2e825c2f273bbe3e9226264
Jason Reilly <jdr0887@gmail.com>
2016-02-19 10:56:07 -0500

    adding service annotations

--- mapseq-dao/mapseq-dao-jpa/src/main/java/edu/unc/mapseq/dao/jpa/AttributeDAOImpl.java
+++ mapseq-dao/mapseq-dao-jpa/src/main/java/edu/unc/mapseq/dao/jpa/AttributeDAOImpl.java
@@ -1,8 +1,16 @@
 package edu.unc.mapseq.dao.jpa;
 
+import javax.inject.Singleton;
+import javax.transaction.Transactional;
+
+import org.ops4j.pax.cdi.api.OsgiServiceProvider;
+
 import edu.unc.mapseq.dao.AttributeDAO;
 import edu.unc.mapseq.dao.model.Attribute;
 
+@OsgiServiceProvider(classes = { AttributeDAO.class })
+@Transactional(Transactional.TxType.SUPPORTS)
+@Singleton
 public class AttributeDAOImpl extends BaseDAOImpl<Attribute, Long> implements AttributeDAO {
 
     public AttributeDAOImpl() {
