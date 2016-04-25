28d0d7090ab2452b28b43920971f73acb5e2ad30
Jason Reilly <jdr0887@gmail.com>
2016-04-25 13:44:12 -0400

    adding test

--- mapseq-dao/mapseq-dao-jpa/src/test/java/edu/unc/mapseq/dao/jpa/FileDataDAOImplTest.java
+++ mapseq-dao/mapseq-dao-jpa/src/test/java/edu/unc/mapseq/dao/jpa/FileDataDAOImplTest.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -14,16 +16,20 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import edu.unc.mapseq.dao.MaPSeqDAOException;
+import edu.unc.mapseq.dao.model.Attribute;
 import edu.unc.mapseq.dao.model.FileData;
 import edu.unc.mapseq.dao.model.Flowcell;
 import edu.unc.mapseq.dao.model.Job;
 import edu.unc.mapseq.dao.model.MimeType;
+import edu.unc.mapseq.dao.model.Sample;
 
 public class FileDataDAOImplTest {
 
@@ -35,6 +41,12 @@ public class FileDataDAOImplTest {
     public static void setup() {
         emf = Persistence.createEntityManagerFactory("test-mapseq", null);
         em = emf.createEntityManager();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        em.close();
+        emf.close();
     }
 
     @Test
@@ -58,6 +70,88 @@ public class FileDataDAOImplTest {
         } catch (MaPSeqDAOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void fixNCGenesPaths() throws MaPSeqDAOException {
+
+        StudyDAOImpl studyDAO = new StudyDAOImpl();
+        studyDAO.setEntityManager(em);
+
+        FlowcellDAOImpl flowcellDAO = new FlowcellDAOImpl();
+        flowcellDAO.setEntityManager(em);
+
+        SampleDAOImpl sampleDAO = new SampleDAOImpl();
+        sampleDAO.setEntityManager(em);
+
+        FileDataDAOImpl fileDataDAO = new FileDataDAOImpl();
+        fileDataDAO.setEntityManager(em);
+
+        AttributeDAOImpl attributeDAO = new AttributeDAOImpl();
+        attributeDAO.setEntityManager(em);
+
+        List<Flowcell> flowcells = flowcellDAO.findByStudyId(2L);
+
+        if (CollectionUtils.isNotEmpty(flowcells)) {
+            for (Flowcell flowcell : flowcells) {
+
+                flowcell.setBaseDirectory("/projects/sequence_analysis/medgenwork/NC_GENES/BCL");
+                flowcell.setAttributes(new HashSet<Attribute>());
+                flowcell.setFileDatas(new HashSet<FileData>());
+                em.getTransaction().begin();
+                flowcellDAO.save(flowcell);
+                em.getTransaction().commit();
+
+                Attribute attribute = new Attribute("readCount", "2");
+                attribute.setId(attributeDAO.save(attribute));
+                flowcell.getAttributes().add(attribute);
+
+                FileData sampleSheetFileData = new FileData(String.format("%s.csv", flowcell.getName()),
+                        "/projects/sequence_analysis/medgenwork/NC_GENES/SampleSheets", MimeType.TEXT_CSV);
+                sampleSheetFileData.setId(fileDataDAO.save(sampleSheetFileData));
+                flowcell.getFileDatas().add(sampleSheetFileData);
+                em.getTransaction().begin();
+                flowcellDAO.save(flowcell);
+                em.getTransaction().commit();
+
+                List<Sample> samples = sampleDAO.findByFlowcellId(flowcell.getId());
+                if (CollectionUtils.isNotEmpty(samples)) {
+                    for (Sample sample : samples) {
+
+                        String outputDirectory = sample.getOutputDirectory();
+                        if (StringUtils.isNotEmpty(outputDirectory)) {
+                            sample.setOutputDirectory(outputDirectory.replace("/proj/seq/mapseq/RENCI",
+                                    "/projects/sequence_analysis/medgenwork/NC_GENES/analysis"));
+                        } else {
+                            sample.setOutputDirectory(String.format("%s/%s/L%03d_%s",
+                                    "/projects/sequence_analysis/medgenwork/NC_GENES/analysis",
+                                    sample.getFlowcell().getName(), sample.getLaneIndex(), sample.getBarcode()));
+                        }
+
+                        Set<FileData> sampleFileDataSet = sample.getFileDatas();
+
+                        if (CollectionUtils.isNotEmpty(sampleFileDataSet)) {
+                            for (FileData fileData : sampleFileDataSet) {
+                                String path = fileData.getPath();
+                                fileData.setPath(path.replace("/proj/seq/mapseq/RENCI",
+                                        "/projects/sequence_analysis/medgenwork/NC_GENES/analysis"));
+                                em.getTransaction().begin();
+                                fileDataDAO.save(fileData);
+                                em.getTransaction().commit();
+
+                            }
+                        }
+
+                        em.getTransaction().begin();
+                        sampleDAO.save(sample);
+                        em.getTransaction().commit();
+
+                    }
+                }
+
+            }
+        }
+
     }
 
     @Test
@@ -146,9 +240,4 @@ public class FileDataDAOImplTest {
 
     }
 
-    @AfterClass
-    public static void tearDown() {
-        em.close();
-        emf.close();
-    }
 }
