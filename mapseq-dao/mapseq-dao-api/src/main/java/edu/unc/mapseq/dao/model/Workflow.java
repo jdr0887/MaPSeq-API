f38912a9acf1b4feb6da0673a98872d0036fd07b
Jason Reilly <jdr0887@gmail.com>
2016-07-11 08:30:12 -0400

    adding system

--- mapseq-dao/mapseq-dao-api/src/main/java/edu/unc/mapseq/dao/model/Workflow.java
+++ mapseq-dao/mapseq-dao-api/src/main/java/edu/unc/mapseq/dao/model/Workflow.java
@@ -3,7 +3,10 @@ package edu.unc.mapseq.dao.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -32,14 +35,19 @@ public class Workflow extends DictionaryEntity {
     @OneToMany(mappedBy = "workflow", fetch = FetchType.LAZY)
     private Set<WorkflowRun> workflowRuns;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "system")
+    private WorkflowSystemType system;
+
     public Workflow() {
         super();
         this.workflowRuns = new HashSet<>();
     }
 
-    public Workflow(String name) {
+    public Workflow(String name, WorkflowSystemType system) {
         super(name);
         this.workflowRuns = new HashSet<>();
+        this.system = system;
     }
 
     public Set<WorkflowRun> getWorkflowRuns() {
@@ -50,9 +58,39 @@ public class Workflow extends DictionaryEntity {
         this.workflowRuns = workflowRuns;
     }
 
+    public WorkflowSystemType getSystem() {
+        return system;
+    }
+
+    public void setSystem(WorkflowSystemType system) {
+        this.system = system;
+    }
+
     @Override
     public String toString() {
-        return String.format("Workflow [id=%s, name=%s, created=%s]", id, name, created);
+        return String.format("Workflow [id=%s, name=%s, created=%s, system=%s]", id, name, created, system);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((system == null) ? 0 : system.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Workflow other = (Workflow) obj;
+        if (system != other.system)
+            return false;
+        return true;
     }
 
 }
