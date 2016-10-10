00ec6a904be10a0e5d7000582bf3b44880f8db02
Jason Reilly <jdr0887@gmail.com>
2016-10-10 15:57:17 -0400

    removing persisting of state during monitor of condor

--- mapseq-workflow-api/src/main/java/edu/unc/mapseq/workflow/StartCondorMonitor.java
+++ mapseq-workflow-api/src/main/java/edu/unc/mapseq/workflow/StartCondorMonitor.java
@@ -9,73 +9,60 @@ import org.renci.jlrm.condor.cli.CondorLookupDAGStatusCallable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import edu.unc.mapseq.dao.model.WorkflowRunAttemptStatusType;
-
 public class StartCondorMonitor implements Runnable {
 
     private final Logger logger = LoggerFactory.getLogger(StartCondorMonitor.class);
-
-    private WorkflowExecutor pipelineExecutor;
 
     private CondorJob jobNode;
 
     private boolean jobFinished = false;
 
-    public StartCondorMonitor(WorkflowExecutor pipelineExecutor, CondorJob jobNode) {
+    private String message;
+
+    public StartCondorMonitor(CondorJob jobNode) {
         super();
-        this.pipelineExecutor = pipelineExecutor;
         this.jobNode = jobNode;
     }
 
     @Override
     public void run() {
         logger.debug("ENTERING run()");
-        CondorJobStatusType statusType = null;
 
         try {
             // this parses the dagman.out file...ie, more error prone & subject to log format changes per condor version
             File dagmanOutFile = new File(jobNode.getSubmitFile().getParentFile(),
                     jobNode.getSubmitFile().getName().replace(".dag", ".dag.dagman.out"));
-            CondorLookupDAGStatusCallable lookupStatus = new CondorLookupDAGStatusCallable(dagmanOutFile);
 
             // this shells out to run condor_q
+            CondorLookupDAGStatusCallable lookupStatus = new CondorLookupDAGStatusCallable(dagmanOutFile);
             // CondorLookupStatusCallable lookupStatus = new CondorLookupStatusCallable(jobNode);
 
-            statusType = lookupStatus.call();
-        } catch (Exception e) {
-            logger.warn("Error", e);
-        }
+            CondorJobStatusType statusType = lookupStatus.call();
 
-        if (statusType == null) {
-            return;
-        }
+            if (statusType == null) {
+                return;
+            }
 
-        logger.debug("Job status: {}", StringUtils.capitalize(statusType.toString()));
+            logger.debug("Job status: {}", StringUtils.capitalize(statusType.toString()));
 
-        // go here for codes http://www.cs.wisc.edu/~adesmet/status.html
-        try {
+            // go here for codes http://www.cs.wisc.edu/~adesmet/status.html
             switch (statusType.getCode()) {
                 case 1:
-                    pipelineExecutor.setWorkflowStatus(new WorkflowRunStatusInfo(WorkflowRunAttemptStatusType.PENDING));
                     jobFinished = false;
                     break;
                 case 2:
-                    pipelineExecutor.setWorkflowStatus(new WorkflowRunStatusInfo(WorkflowRunAttemptStatusType.RUNNING));
                     jobFinished = false;
                     break;
                 case 3:
-                    pipelineExecutor.setWorkflowStatus(
-                            new WorkflowRunStatusInfo(WorkflowRunAttemptStatusType.FAILED, "Removed from HTCondor"));
+                    message = "Removed from HTCondor";
                     jobFinished = true;
                     break;
                 case 4:
-                    pipelineExecutor.setWorkflowStatus(
-                            new WorkflowRunStatusInfo(WorkflowRunAttemptStatusType.DONE, "Completed in HTCondor"));
+                    message = "Completed in HTCondor";
                     jobFinished = true;
                     break;
                 case 5:
-                    pipelineExecutor.setWorkflowStatus(
-                            new WorkflowRunStatusInfo(WorkflowRunAttemptStatusType.HELD, "Held in HTCondor"));
+                    message = "Held in HTCondor";
                     jobFinished = true;
                     break;
                 default:
@@ -87,20 +74,6 @@ public class StartCondorMonitor implements Runnable {
             jobFinished = true;
         }
 
-        // if (!jobFinished) {
-        // MaPSeqDAOBean daoBean = pipelineExecutor.getWorkflow().getWorkflowBeanService().getMaPSeqDAOBean();
-        // try {
-        // WorkflowRunAttempt attempt = daoBean.getWorkflowRunAttemptDAO().findById(
-        // pipelineExecutor.getWorkflow().getWorkflowRunAttempt().getId());
-        // if (attempt.getStatus().equals(WorkflowRunAttemptStatusType.RESET)) {
-        // pipelineExecutor.setWorkflowStatus(WorkflowRunAttemptStatusType.DONE);
-        // jobFinished = true;
-        // }
-        // } catch (MaPSeqDAOException e) {
-        // e.printStackTrace();
-        // }
-        // }
-
     }
 
     public boolean isJobFinished() {
@@ -109,6 +82,14 @@ public class StartCondorMonitor implements Runnable {
 
     public void setJobFinished(boolean jobFinished) {
         this.jobFinished = jobFinished;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 
 }
